07610b0ad434aa5e7b73915b586f7458dae57def
Sunho Kim <supersunho@gmail.com>
2021-11-26 18:01:00 +0900

    20211126180100 commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Post: Standard 2",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["React"],
+        "tags": ["Dev","React","React Native","Android"],
+        "url": "/react/test-1/",
+        "teaser": null
+      },{
+        "title": "Post: Standard 1",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["React"],
+        "tags": ["Dev","React","React Native","Android"],
+        "url": "/react/test/",
+        "teaser": null
+      }]
